dd6ebcf68d0d97dd790c42c0d7ce96e335f75048
Depayss <Christophermaomao@gmail.com>
2025-05-21 21:13:57 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import Header from './components/Header';
 import Entry from './components/Entry';
+import data from './data';
 import './App.css';
+import './components/Entry.css'
 
 function App() {
+
+  // implementing information from data.js to each Entry component
+  const entryData = data.map((info)=>{
+    return (
+      <Entry
+
+      // the Entry component is expecting data to fill its props. 
+        img = {info.img} // the src and alt properties are auto-filled
+        title = {info.title}
+        country = {info.country}
+        googleMapsLink = {info.googleMapsLink}
+        dates = {info.dates}
+        text = {info.text}
+      />
+    )
+  })
+
   return (
     <>
       <Header />
-      <Entry />
+      <main>
+        {entryData}
+      </main>
     </>
     
   );
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://media.discordapp.net/attachments/759795616539738156/1374807754883203104/fuji.jpg?ex=682f64ff&is=682e137f&hm=8639a5e507ffdcbb254a09aeb994cfc602d419484107c710244723ed7a2ae899&=&format=webp&width=590&height=865",
+            alt: "Mount Fuji"
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://cdn.discordapp.com/attachments/759795616539738156/1374807755155837129/opera.jpg?ex=682f64ff&is=682e137f&hm=10befa538ec8191e440eef33b08ab0acb334a02cf9d4c00fdac33089fdcc04b0&",
+            alt: "Sydney Opera House"
+        },
+        title: "Sydney Opera House",
+        country: "Australia",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://media.discordapp.net/attachments/759795616539738156/1374807754597728317/fjord.jpg?ex=682f64ff&is=682e137f&hm=def9521604f1ee1b985afaae7cd911a81e868f85904679964eab22dc894c22ce&=&format=webp&width=590&height=865",
+            alt: "Geirangerfjord"
+        },
+        title: "Geirangerfjord",
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    },
+]
